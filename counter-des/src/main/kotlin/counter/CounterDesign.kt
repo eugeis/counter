5aa5dea205c9934606071bacc19dd7522ec98b86
Eugen Eisler <eoeisler@gmail.com>
2019-01-31 23:41:23 +0100

    Work on StateMachine

--- counter-des/src/main/kotlin/counter/CounterDesign.kt
+++ counter-des/src/main/kotlin/counter/CounterDesign.kt
@@ -3,12 +3,14 @@ package counter
 import ee.design.*
 import ee.lang.*
 
-object Counter : Module({ namespace("ee.counter") }) {
-    object Counter : Entity() {
-        val id = propS().unique()
-        val count = propL()
+object Counter : Comp({ namespace("ee.counter") }) {
+    object Counter : Module() {
+        object Counter : Entity() {
+            val id = propS().unique()
+            val count = propL()
 
-        val increment = updateBy(p(count) { value(count) })
+            val increment = updateBy(p(count) { value(count) })
 
+        }
     }
 }
